3073a0b61ec60f481729e7f4d659c6c3f8b00028
emilia98 <emilia_nedialkova@abv.bg>
2017-12-31 12:11:38 +0200

    Added ShoppingList Project with ASP.NET and some validations

---
+++ Tech Module/ST - Exams/ShoppingList/C# Solution/ShoppingList/Controllers/ProductController.cs
@@ -0,0 +1,98 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using ShoppingList.Models;
+
+namespace ShoppingList.Controllers
+{
+    [ValidateInput(false)]
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            using (var db = new ShoppingListDbContext())
+            {
+                var products = db.Products.ToList();
+                return View(products);
+            }
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            return View("Create", new Product());
+        }
+
+        [HttpPost]
+        [Route("create")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product)
+        {
+            using (var db = new ShoppingListDbContext())
+            {
+                if(ModelState.IsValid)
+                {
+                    db.Products.Add(product);
+                    db.SaveChanges();
+
+                    return Redirect("/");
+                }
+
+                return View(product);
+            }
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            using (var db = new ShoppingListDbContext())
+            {
+                var product = db.Products.Find(id);
+
+                if (product == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(product);
+            }
+        }
+
+        [HttpPost]
+        [Route("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditConfirm(int? id, Product productModel)
+        {
+            using (var db = new ShoppingListDbContext())
+            {
+                var productFromDb = db.Products.Find(id);
+
+                if (productFromDb == null)
+                {
+                    return HttpNotFound();
+                }
+
+                if(ModelState.IsValid)
+                {
+                    productFromDb.Priority = productModel.Priority;
+                    productFromDb.Name = productModel.Name;
+                    productFromDb.Quantity = productModel.Quantity;
+                    productFromDb.Status = productModel.Status;
+
+                    db.SaveChanges();
+                    return Redirect("/");
+                }
+
+                return View("Edit", productModel);
+            }
+        }
+    }
+}
---
+++ Tech Module/ST - Exams/ShoppingList/C# Solution/ShoppingList/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingList.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int Priority { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public string Name { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+    }
+}
