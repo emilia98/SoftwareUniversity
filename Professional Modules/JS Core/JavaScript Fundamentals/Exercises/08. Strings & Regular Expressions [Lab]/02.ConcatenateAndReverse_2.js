aa04664771f2dc4bf4ce3e6fb27f8ebbf657a6b9
emilia98 <emilia_nedialkova@abv.bg>
2018-01-26 16:51:49 +0200

    Removed a folder with incomplete files

--- Professional Modules/JS Core/JavaScript Fundamentals/Exercises/08. Strings & Regular Expressions [Lab]/02.ConcatenateAndReverse_2.js
+++
@@ -1,8 +0,0 @@
-function concatAndReverse(array) {
-   return Array.from(array.join('')).reverse().join('');
-}
-
-/* Uncomment to test:
-console.log(concatAndReverse(['I', 'am', 'student']));
-console.log(concatAndReverse(['race', 'car']));
-*/
